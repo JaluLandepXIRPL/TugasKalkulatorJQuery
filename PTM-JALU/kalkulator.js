020f9c74221a6b421be5943b95d857ba24cdbb20
JaluLandepXIRPL <87351248+JaluLandepXIRPL@users.noreply.github.com>
2022-02-01 08:12:13 +0700

    Add files via upload

---
+++ PTM-JALU/kalkulator.js
@@ -0,0 +1,27 @@
+tambah.onclick = function(){
+    let a  = $('#bill1').val();
+    let b  = $('#bill2').val();
+    let c = parseFloat(a) + parseFloat(b);
+      $("#hasil").html (c); ;
+}
+
+kurang.onclick = function (){
+    let a = $('#bill1').val();
+    let b = $('#bill2').val();
+    let c = parseFloat(a) + parseFloat(b);
+     $('#hasil') .html (c);
+}
+
+bagi.onclick = function (){
+    let a = $('#bill1').val();
+    let b = $('#bill2').val();
+    let c = parseFloat(a) + parseFloat(b);
+    $('#hasil').html (c);
+}
+
+kali.onclick = function () {
+    let a = $ ('#bill1').val();
+    let b = $ ('#bill2').val();
+    let c = parseFloat(a) + parseFloat(b);
+    $('#hasil').html (c);
+}
